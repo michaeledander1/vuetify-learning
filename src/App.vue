9a01f6071ebe741f5222b3bbaff52bf51a4b1586
michaeledander1 <michaeledander1@gmail.com>
2019-01-06 22:24:48 +0100

    about section added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <Header />
-    <v-content>
+    <v-content class="mx-4">
       <router-view></router-view>
     </v-content>
   </v-app>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-container class="my-5">
+      Michael Blah Blah
+    </v-container>
   </div>
 </template>
